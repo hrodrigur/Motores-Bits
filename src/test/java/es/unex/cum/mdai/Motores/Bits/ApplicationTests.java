f42c28a68390a31a26dfcda3e73dcb184bce93e7
hrodrigur <hrodrigur@alumnos.unex.es>
2025-10-23 15:41:04 +0200

    Initial commit

---
+++ src/test/java/es/unex/cum/mdai/Motores/Bits/ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unex.cum.mdai.Motores.Bits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
